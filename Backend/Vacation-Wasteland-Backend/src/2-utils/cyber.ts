54cccab6b846ab14746ec5e761083dd2abed2c96
Ido Waisbart <idowaisbart@gmail.com>
2026-04-11 20:49:01 +0300

    A bit of progress on the backend

---
+++ Backend/Vacation-Wasteland-Backend/src/2-utils/cyber.ts
@@ -0,0 +1,52 @@
+import { SignOptions } from "jsonwebtoken";
+import { UserModel } from "../3-models/user-model";
+import jwt from "jsonwebtoken";
+import { appConfig } from "./app-config";
+import { Role } from "../3-models/enums";
+
+class Cyber {
+
+    public generateToken(user: UserModel): string {
+
+        // Create payload: 
+        const payload = { user };
+
+        // Create options: 
+        const options: SignOptions = { expiresIn: "3h" };
+
+        // Generate token: 
+        const token = jwt.sign(payload, appConfig.jwtSecret, options);
+
+        // Return token: 
+        return token;
+    }
+
+    // Verify legal token:
+    public verifyToken(token: string): boolean {
+        try {
+            if (!token) return false;
+            jwt.verify(token, appConfig.jwtSecret);
+            return true;
+        }
+        catch {
+            return false;
+        }
+    }
+
+    // Verify admin:
+    public verifyAdmin(token: string): boolean {
+        try {
+            if (!token) return false;
+            jwt.verify(token, appConfig.jwtSecret);
+            const payload = jwt.decode(token) as { user: UserModel };
+            const user = payload.user;
+            return user.roleId === Role.Admin;
+        }
+        catch {
+            return false;
+        }
+    }
+
+}
+
+export const cyber = new Cyber();
---
+++ Backend/Vacation-Wasteland-Backend/src/6-middleware/security-middleware.ts
@@ -0,0 +1,50 @@
+import { NextFunction, Request, Response } from "express";
+import { cyber } from "../2-utils/cyber";
+import { UnauthorizedError } from "../3-models/errors";
+
+class SecurityMiddleware {
+
+    // Verify legal token:
+    public verifyToken(request: Request, response: Response, next: NextFunction): void {
+
+        // Extract token: 
+        // Authorization: Bearer the-token...
+        //                       -->
+        //                01234567
+        const authorization = request.headers.authorization;
+        const token = authorization?.substring(7)!;
+
+        // If token not legal:
+        if (!cyber.verifyToken(token)) {
+            next(new UnauthorizedError("You are not logged-in."));
+            return;
+        }
+
+        // All is good: 
+        next();
+    }
+
+    // Verify admin:
+    public verifyAdmin(request: Request, response: Response, next: NextFunction): void {
+
+        // Extract token: 
+        // Authorization: Bearer the-token...
+        //                       -->
+        //                01234567
+        const authorization = request.headers.authorization;
+        const token = authorization?.substring(7)!;
+
+        // If user is not admin:
+        if (!cyber.verifyAdmin(token)) {
+            next(new UnauthorizedError("You are not authorized."));
+            return;
+        }
+
+        // All is good: 
+        next();
+    }
+
+
+}
+
+export const securityMiddleware = new SecurityMiddleware();
