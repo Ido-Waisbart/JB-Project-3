74e9e5defe03bbc13efd6c699aeafbb96e9315c1
Ido Waisbart <idowaisbart@gmail.com>
2026-04-24 08:35:39 +0300

    Vacations Endpoints

--- Backend/Vacation-Wasteland-Backend/src/4-services/vacation-service.ts
+++ Backend/Vacation-Wasteland-Backend/src/4-services/vacation-service.ts
@@ -1,8 +1,23 @@
+import { fileSaver } from "uploaded-file-saver";
 import { appConfig } from "../2-utils/app-config";
 import { dal } from "../2-utils/dal";
+import { ResourceNotFoundError } from "../3-models/client-errors";
 import { VacationModel } from "../3-models/vacation-model";
+import { OkPacketParams } from "mysql2";
 
 class VacationService {
+  // TODO: Move elsewhere? Utils?
+  public toMySQLDateLocal(date: Date): string {
+    date = new Date(date);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+  }
+
+  //  Get all vacation.
+  //  √
   public async getAllVacations(): Promise<VacationModel[] | undefined> {
     // Debug data:
     // let v: VacationModel = {id: 1, destination: "Somewhere in Nevada", description: "USA", start_date: new Date(), end_date: new Date(), price_in_usd: 799};
@@ -23,10 +38,147 @@ class VacationService {
     return vacations;
   }
 
+  //  Get one vacation.
+  //  TODO: Is this even used?
+  public async getOneVacation(id: number): Promise<VacationModel> {
+    // Create sql:
+    const sql =
+      "select *, concat(?, image_uri) as image_url from vacations where id = ?";
+    const values = [appConfig.imagesLocation, id];
+
+    // Execute:
+    const vacations = (await dal.execute(sql, values)) as VacationModel[];
+
+    // Extract the single vacation:
+    const vacation = vacations[0];
+
+    // If no such vacation:
+    if (!vacation) throw new ResourceNotFoundError(id);
+
+    // Return:
+    return vacation;
+  }
+
+  //  Add vacation:
+  public async addVacation(vacation: VacationModel): Promise<VacationModel> {
+    // Validation:
+    vacation.validate();
+
+    // Save the image:
+    const image_name = vacation.image
+      ? await fileSaver.add(vacation.image)
+      : null!;
+
+    // Create sql:
+    const sql =
+      "insert into vacations(destination, description, price_in_usd, start_date, end_date, image_uri) values(?, ?, ?, ?, ?, ?)";
+    const values = [
+      vacation.destination,
+      vacation.description ?? null,
+      vacation.price_in_usd,
+      this.toMySQLDateLocal(vacation.start_date),
+      this.toMySQLDateLocal(vacation.end_date),
+      image_name,
+    ];
+
+    // Execute:
+    const info: OkPacketParams = (await dal.execute(
+      sql,
+      values,
+    )) as OkPacketParams;
+
+    // Get the added vacation from the database:
+    const dbVacation = await this.getOneVacation(info.insertId!);
+
+    // Return:
+    return dbVacation;
+  }
+
+  //  Update vacation:
+  public async updateVacation(vacation: VacationModel): Promise<VacationModel> {
+    // Validation:
+    vacation.validate();
+
+    // Update image:
+    const oldImageName = await this.getImageName(vacation.id);
+    const newImageName = vacation.image
+      ? await fileSaver.update(oldImageName!, vacation.image)
+      : oldImageName;
+
+    // Create sql:
+    const sql =
+      "update vacations set destination = ?, description = ?, price_in_usd = ?, start_date = ?, end_date = ?, image_uri = ? where id = ?";
+    const values = [
+      vacation.destination,
+      vacation.description ?? null,
+      vacation.price_in_usd,
+      this.toMySQLDateLocal(vacation.start_date),
+      this.toMySQLDateLocal(vacation.end_date),
+      newImageName,
+      vacation.id,
+    ];
+
+    // Execute:
+    const info: OkPacketParams = (await dal.execute(
+      sql,
+      values,
+    )) as OkPacketParams;
+
+    // If no such vacation:
+    if (info.affectedRows === 0) throw new ResourceNotFoundError(vacation.id!);
+
+    // Get the updated vacation from the database:
+    const dbVacation = await this.getOneVacation(vacation.id!);
+
+    // Return:
+    return dbVacation;
+  }
+
+  //  Delete vacation:
+  public async deleteVacation(id: number): Promise<void> {
+    // Get old image:
+    const oldImageName = await this.getImageName(id);
+
+    // Create sql:
+    const sql = "delete from vacations where id = ?";
+    const values = [id];
+
+    // Execute:
+    const info: OkPacketParams = (await dal.execute(
+      sql,
+      values,
+    )) as OkPacketParams;
+
+    // If no such vacation:
+    if (info.affectedRows === 0) throw new ResourceNotFoundError(id);
+
+    // Delete image:
+    await fileSaver.delete(oldImageName!);
+  }
+
+  //  TODO
+  //  Get vacations by price range:
+  public async getVacationsByPriceRange(
+    min: number,
+    max: number,
+  ): Promise<VacationModel[]> {
+    // Create sql:
+    const sql =
+      "select * from vacations where price between ? and ? order by price";
+    const values = [min, max];
+
+    // Execute:
+    const vacations = (await dal.execute(sql, values)) as VacationModel[];
+
+    // Return:
+    return vacations;
+  }
+
   // Get image name from db.
   // Used only within this file, vacation-service.ts.
+  //  √
   private async getImageName(id: number): Promise<string | null> {
-    const sql = "select image_uri from products where id = ?";
+    const sql = "select image_uri from vacations where id = ?";
     const values = [id];
     const vacations = (await dal.execute(sql, values)) as VacationModel[];
     const vacation = vacations[0];
--- Backend/Vacation-Wasteland-Backend/src/5-controllers/vacation-controller.ts
+++ Backend/Vacation-Wasteland-Backend/src/5-controllers/vacation-controller.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response, Router } from "express";
 import { vacation_service } from "../4-services/vacation-service";
 import { fileSaver } from "uploaded-file-saver";
+import { VacationModel } from "../3-models/vacation-model";
+import { StatusCode } from "../3-models/enums";
+import { securityMiddleware } from "../6-middleware/security-middleware";
 
 class VacationController {
   public router: Router = express.Router();
@@ -8,7 +11,15 @@ class VacationController {
   public constructor() {
     // TODO: Choose which API routes here and in users and maybe in likes can be accessed by users/admins only. Keep the routes untokened for ease of development.
     this.router.get("/api/vacations", this.getVacations);
-    this.router.get("/api/vacations/images/:imageName", this.getImage);  // TODO: Where is this used? Answer: IMAGES_LOCATION = BE's appConfig.imagesLocation = "http://localhost:4000/api/vacations/images/"; getAllVacations(), getOneVacation()
+    this.router.get("/api/vacations/:id", this.getOneVacation);
+    this.router.post("/api/vacations", this.addVacation);
+    this.router.put("/api/vacations/:id", this.updateVacation);
+    this.router.delete("/api/vacations/:id", this.deleteVacation);
+    // this.router.post("/api/vacations", securityMiddleware.verifyToken, this.addVacation);
+    // this.router.put("/api/vacations/:id", securityMiddleware.verifyToken, this.updateVacation);
+    // this.router.delete("/api/vacations/:id", securityMiddleware.verifyAdmin, this.deleteVacation);
+    // this.router.get("/api/vacations-by-price/:min/:max", this.getVacationsByPriceRange);
+    this.router.get("/api/vacations/images/:imageName", this.getImage); // TODO: Where is this used? Answer: IMAGES_LOCATION = BE's appConfig.imagesLocation = "http://localhost:4000/api/vacations/images/"; getAllVacations(), getOneVacation()
   }
 
   private async getVacations(request: Request, response: Response) {
@@ -24,6 +35,45 @@ class VacationController {
       });
     }
   }
+
+  // Get one vacation by id:
+  private async getOneVacation(request: Request, response: Response) {
+    const id = +request.params.id;
+    const vacation = await vacation_service.getOneVacation(id);
+    response.json(vacation);
+  }
+
+  // Add vacation:
+  private async addVacation(request: Request, response: Response) {
+    request.body.image = request.files?.image;
+    const vacation = new VacationModel(request.body);
+    const dbVacation = await vacation_service.addVacation(vacation);
+    response.status(StatusCode.Created).json(dbVacation);
+  }
+
+  // Update vacation:
+  private async updateVacation(request: Request, response: Response) {
+    request.body.image = request.files?.image;
+    request.body.id = +request.params.id;
+    const vacation = new VacationModel(request.body);
+    const dbVacation = await vacation_service.updateVacation(vacation);
+    response.json(dbVacation);
+  }
+
+  // Delete vacation:
+  private async deleteVacation(request: Request, response: Response) {
+    const id = +request.params.id;
+    await vacation_service.deleteVacation(id);
+    response.sendStatus(StatusCode.NoContent); // response.status(StatusCode.NoContent).send();
+  }
+
+  // Get vacations by price range:
+  /*private async getVacationsByPriceRange(request: Request, response: Response) {
+    const min = +request.params.min;
+    const max = +request.params.max;
+    const vacations = await vacation_service.getVacationsByPriceRange(min, max);
+    response.json(vacations);
+  }*/
 
   // Get image by imageName:
   private async getImage(request: Request, response: Response) {
