b424851c4599e687c28d934ec1d107ea2bab75ca
Ido Waisbart <idowaisbart@gmail.com>
2026-05-11 10:39:59 +0300

    Remove root package.json and all libraries
    The libraries folder seems to have been made for the PNPM projects, which implies I don't need them.

--- Libraries/Gpt/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { chatGpt } from "./chat-gpt";
--- Libraries/Restify/src/enums.ts
+++
@@ -1,9 +0,0 @@
-export enum StatusCode {
-    OK = 200,
-    Created = 201,
-    NoContent = 204,
-    BadRequest = 400,
-    Unauthorized = 401,
-    NotFound = 404,
-    InternalServerError = 500
-}
